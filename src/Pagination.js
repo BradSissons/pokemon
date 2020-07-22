875673eca496a1f70b8ada3febce20313b9a5427
BradSissons <chillybob1@gmail.com>
2020-07-22 22:40:43 +1200

    Added next and prev function

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Pagination() {
+export default function Pagination({ nextPage, prevPage }) {
     return (
         <div>
-            
+            {prevPage && <button onClick={prevPage}>Previous</button>}
+            {nextPage && <button onClick={nextPage}>Next</button>}
         </div>
     )
 }
