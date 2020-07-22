4b588ea63e09c4c1a53da0be9ce7e2ef58b75d6f
BradSissons <chillybob1@gmail.com>
2020-07-22 22:32:34 +1200

    Added loading function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setLoading(true)
+    axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
-  }, [])
+  }, [currentPageUrl])
 
+  if(loading) return "Loading..."
+  
   return (
     <PokemonList pokemon={pokemon}/>
   );
