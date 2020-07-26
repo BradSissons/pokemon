7ad3519d77c2c1e44ff9013ab05fc2341208b1d8
BradSissons <chillybob1@gmail.com>
2020-07-26 18:41:17 +1200

    Added Pokemon image

--- src/PokemonInfo.js
+++ src/PokemonInfo.js
@@ -6,9 +6,11 @@ export default function PokemonInfo({ pokemon }) {
             {/* if pokemon data is available*/}
             {pokemon &&
             <div>
+                <p>{"Pokedex #" + pokemon.id}</p>
                 <p>{"Name: " + pokemon.name}</p>
-                <p>{"Type: " + (pokemon.types[1] ? pokemon.types[0].type.name + " & " + pokemon.types[1].type.name : pokemon.types[0].type.name)}</p>
-                <p>{"Abilities: " + (pokemon.abilities[2] ? pokemon.abilities[0].ability.name + " & " + pokemon.abilities[1].ability.name + " & " + pokemon.abilities[2].ability.name : pokemon.abilities[0].ability.name + " & " + pokemon.abilities[1].ability.name)}</p>
+                <img src={pokemon.sprites.front_default} alt="Sprite"/>
+                <p>{"Type: " + (pokemon.types[1] ? pokemon.types[0].type.name + " / " + pokemon.types[1].type.name : pokemon.types[0].type.name)}</p>
+                <p>{"Abilities: " + (pokemon.abilities[2] ? pokemon.abilities[0].ability.name + ", " + pokemon.abilities[1].ability.name + ", " + pokemon.abilities[2].ability.name : (pokemon.abilities[1] ? pokemon.abilities[0].ability.name + ", " + pokemon.abilities[1].ability.name : pokemon.abilities[0].ability.name))}</p>
                 <p>{"Weight: " + pokemon.weight / 10 + "kg"}</p>
             </div>
             }
