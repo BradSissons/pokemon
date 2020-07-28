68be3f12b959ddfc09306ff25bad661fde7cfa8c
BradSissons <chillybob1@gmail.com>
2020-07-28 14:09:14 +1200

    Added type colour headers

--- src/PokemonInfo.js
+++ src/PokemonInfo.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Types.css'
 
 export default function PokemonInfo({ pokemon }) {
     function padLeft(ID) {
@@ -14,9 +15,8 @@ export default function PokemonInfo({ pokemon }) {
             {/* if pokemon data is available*/}
             {pokemon &&
             <div>
-            <img src={"https://pokeres.bastionbot.org/images/pokemon/" + pokemon.id + ".png"} alt="Sprite" style={{width:'200px', height:'200px', float:'right'}}/>
-                <p>{"Pokedex #" + padLeft(pokemon.id)}</p>
-                <p>{"Name: " + capitalizeFirstLetter(pokemon.name)}</p>
+                <h1 class={pokemon.types[0].type.name}>{capitalizeFirstLetter(pokemon.name) + " #" + padLeft(pokemon.id)}</h1>
+                <img src={"https://pokeres.bastionbot.org/images/pokemon/" + pokemon.id + ".png"} alt="Sprite" style={{width:'200px', height:'200px', padding:'25px', float:'right'}}/>
                 <p>{"Type: " + (pokemon.types[1] ? capitalizeFirstLetter(pokemon.types[0].type.name) + " / " + capitalizeFirstLetter(pokemon.types[1].type.name) : capitalizeFirstLetter(pokemon.types[0].type.name))}</p>
                 <p>{"Abilities: " + (pokemon.abilities[2] ? capitalizeFirstLetter(pokemon.abilities[0].ability.name) + ", " + capitalizeFirstLetter(pokemon.abilities[1].ability.name) + ", " + capitalizeFirstLetter(pokemon.abilities[2].ability.name) : (pokemon.abilities[1] ? capitalizeFirstLetter(pokemon.abilities[0].ability.name) + ", " + capitalizeFirstLetter(pokemon.abilities[1].ability.name) : capitalizeFirstLetter(pokemon.abilities[0].ability.name)))}</p>
                 <p>{"Weight: " + pokemon.weight / 10 + "kg"}</p>
