f65ae9ec52edd96a1d12308d4ad58d677a9089aa
BradSissons <chillybob1@gmail.com>
2020-07-28 13:37:31 +1200

    Altered image to more consistent icons from secondary source

--- src/PokemonInfo.js
+++ src/PokemonInfo.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
 export default function PokemonInfo({ pokemon }) {
+    function padLeft(ID) {
+        return Array(3 - String(ID).length + 1).join('0') + ID
+    }
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
     return (
         <div>
             {/* if pokemon data is available*/}
             {pokemon &&
             <div>
-                <p>{"Pokedex #" + pokemon.id}</p>
-                <p>{"Name: " + pokemon.name}</p>
-                <img src={pokemon.sprites.front_default} alt="Sprite"/>
-                <p>{"Type: " + (pokemon.types[1] ? pokemon.types[0].type.name + " / " + pokemon.types[1].type.name : pokemon.types[0].type.name)}</p>
-                <p>{"Abilities: " + (pokemon.abilities[2] ? pokemon.abilities[0].ability.name + ", " + pokemon.abilities[1].ability.name + ", " + pokemon.abilities[2].ability.name : (pokemon.abilities[1] ? pokemon.abilities[0].ability.name + ", " + pokemon.abilities[1].ability.name : pokemon.abilities[0].ability.name))}</p>
+            <img src={"https://pokeres.bastionbot.org/images/pokemon/" + pokemon.id + ".png"} alt="Sprite" style={{width:'200px', height:'200px', float:'right'}}/>
+                <p>{"Pokedex #" + padLeft(pokemon.id)}</p>
+                <p>{"Name: " + capitalizeFirstLetter(pokemon.name)}</p>
+                <p>{"Type: " + (pokemon.types[1] ? capitalizeFirstLetter(pokemon.types[0].type.name) + " / " + capitalizeFirstLetter(pokemon.types[1].type.name) : capitalizeFirstLetter(pokemon.types[0].type.name))}</p>
+                <p>{"Abilities: " + (pokemon.abilities[2] ? capitalizeFirstLetter(pokemon.abilities[0].ability.name) + ", " + capitalizeFirstLetter(pokemon.abilities[1].ability.name) + ", " + capitalizeFirstLetter(pokemon.abilities[2].ability.name) : (pokemon.abilities[1] ? capitalizeFirstLetter(pokemon.abilities[0].ability.name) + ", " + capitalizeFirstLetter(pokemon.abilities[1].ability.name) : capitalizeFirstLetter(pokemon.abilities[0].ability.name)))}</p>
                 <p>{"Weight: " + pokemon.weight / 10 + "kg"}</p>
             </div>
             }
